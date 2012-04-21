84018937f218782516ace493edd22cca74340acd
Matthew A. Miller <linuxwolf@outer-planes.net>
2012-04-20 20:09:35 -0600

    update deps to latest stable

--- src/header.js
+++ src/header.js
@@ -3,8 +3,8 @@ function create(window) {
   var location, navigator, XMLHttpRequest;
 
   window = window || require('jsdom').jsdom().createWindow();
-  location = window.location || require('location');
-  navigator = window.navigator || require('navigator');
+  window.location = window.location || require('location');
+  window.navigator = window.navigator || require('navigator');
 
   if (!window.XMLHttpRequest && 'function' !== typeof window.ActiveXObject) {
     window.XMLHttpRequest = require('xmlhttprequest'); // require('XMLHttpRequest');

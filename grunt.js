6ccfcb8de004e7f4a124104673b75ff77e2fcbbd
Spike Brehm <ocelot@gmail.com>
2012-11-05 16:34:38 -0800

    bump to 1.8.2 from 1.7.2

--- grunt.js
+++ grunt.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
       http = require('http'),
       fs = require('fs'),
       host = 'ajax.googleapis.com',
-      jqPath = '/ajax/libs/jquery/1.7.2/jquery.js';
+      jqPath = '/ajax/libs/jquery/1.8.2/jquery.js';
 
   grunt.registerTask('build', 'builds jquery module for use in node', function() {
     var tmpDir = './tmp', distDir = './lib',
